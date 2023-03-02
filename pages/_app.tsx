5dbb1bd25496aacb51eb47f826dba3ad1cb3fe71
John <johnacreps@gmail.com>
2023-03-02 14:34:53 -0600

    Fixing build issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,26 @@
-import { FC, useEffect, useRef, useState } from 'react';
-import { ThemeProvider } from 'styled-components';
-import type { AppProps } from 'next/app';
-import axios from 'axios';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import { Provider as ReduxProvider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { Hydrate } from 'react-query/hydration';
-import App from '../components/App';
-import { store } from '../store';
-import AuthPopup from '../components/Auth';
-import currentTheme, { Theme } from '../theme';
-import { Config } from '../utils';
+import { FC, useEffect, useRef, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import type { AppProps } from "next/app";
+import axios from "axios";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { Provider as ReduxProvider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Hydrate } from "react-query/hydration";
+import App from "../components/App";
+import { store } from "../store";
+import AuthPopup from "../components/Auth";
+import currentTheme, { Theme } from "../theme";
+import { Config } from "../utils";
 
 axios.defaults.baseURL = Config.API_URL;
 axios.defaults.withCredentials = true;
 
-const NextApp: FC<AppProps> = ({ Component, pageProps }) => {
+const NextApp: FC<AppProps> = ({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: unknown }>) => {
   const [theme, setTheme] = useState<Theme>(null);
   const queryClientRef = useRef(null);
   if (!queryClientRef.current) {
@@ -35,15 +38,15 @@ const NextApp: FC<AppProps> = ({ Component, pageProps }) => {
     const nProgressDone = () => NProgress.done();
 
     // N Progress.
-    Router.events.on('routeChangeStart', nProgressStart);
-    Router.events.on('routeChangeComplete', nProgressDone);
-    Router.events.on('routeChangeError', nProgressDone);
+    Router.events.on("routeChangeStart", nProgressStart);
+    Router.events.on("routeChangeComplete", nProgressDone);
+    Router.events.on("routeChangeError", nProgressDone);
 
     return () => {
       // N Progress.
-      Router.events.on('routeChangeStart', nProgressStart);
-      Router.events.on('routeChangeComplete', nProgressDone);
-      Router.events.on('routeChangeError', nProgressDone);
+      Router.events.on("routeChangeStart", nProgressStart);
+      Router.events.on("routeChangeComplete", nProgressDone);
+      Router.events.on("routeChangeError", nProgressDone);
     };
   }, []);
 
