b7de75791be5423af65358339dfbdda695990f3e
John <johnacreps@gmail.com>
2023-03-02 14:43:02 -0600

    Updating production port

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,8 @@
-const API_PRODUCTION_URL = 'http://localhost:4000';
-const API_DEV_URL = 'http://localhost:4000';
+const API_PRODUCTION_URL = "http://localhost:8080";
+const API_DEV_URL = "http://localhost:4000";
 
 export const Config = {
-  API_URL: process.env.NODE_ENV === 'production' ? API_PRODUCTION_URL : API_DEV_URL,
-  GOOGLE_ANALYTICS_ID: 'G-MD706N8TZV',
+  API_URL:
+    process.env.NODE_ENV === "production" ? API_PRODUCTION_URL : API_DEV_URL,
+  GOOGLE_ANALYTICS_ID: "G-MD706N8TZV",
 };
