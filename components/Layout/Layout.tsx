6776c06be67f0a1231d97bd4da8c2a05d99440ef
John <johnacreps@gmail.com>
2023-03-07 15:13:28 -0600

    Remove Annoucement

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,13 +1,12 @@
-import { FC, useEffect, useRef, useState } from 'react';
-import Router from 'next/router';
-import { Root, StyledContainer } from './style';
-import Header from '../Header';
-import SideBar from '../Sidebar';
-import { Cookies, getCookie, useClickOutside } from '../../utils';
-import { Spacing, Screen } from '../../theme';
-import RightSideBar from '../RightSideBar';
-import Seo from '../Seo';
-import Announcement from '../Announcement';
+import { FC, useEffect, useRef, useState } from "react";
+import Router from "next/router";
+import { Root, StyledContainer } from "./style";
+import Header from "../Header";
+import SideBar from "../Sidebar";
+import { Cookies, getCookie, useClickOutside } from "../../utils";
+import { Spacing, Screen } from "../../theme";
+import RightSideBar from "../RightSideBar";
+import Seo from "../Seo";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -17,9 +16,13 @@ interface LayoutProps {
   marginTop?: Spacing;
 }
 
-const Layout: FC<LayoutProps> = ({ children, hideLeftSidebar, hideRightSidebar, containerMaxWidth, marginTop }) => {
-  const isAnnouncementDisabled = getCookie(Cookies.Announcement_Disabled);
-  const [isAnnouncementOpen, setIsAnnouncementOpen] = useState(isAnnouncementDisabled !== 'true');
+const Layout: FC<LayoutProps> = ({
+  children,
+  hideLeftSidebar,
+  hideRightSidebar,
+  containerMaxWidth,
+  marginTop,
+}) => {
   const sideBarRef = useRef(null);
   const hamburgerRef = useRef(null);
   const [isSideBarOpen, setIsSidebarOpen] = useState(false);
@@ -30,7 +33,7 @@ const Layout: FC<LayoutProps> = ({ children, hideLeftSidebar, hideRightSidebar, 
   const toggleSidebar = () => setIsSidebarOpen(!isSideBarOpen);
 
   useEffect(() => {
-    Router.events.on('routeChangeComplete', () => {
+    Router.events.on("routeChangeComplete", () => {
       if (isSideBarOpen) {
         setIsSidebarOpen(false);
       }
@@ -40,10 +43,11 @@ const Layout: FC<LayoutProps> = ({ children, hideLeftSidebar, hideRightSidebar, 
   return (
     <>
       <Seo />
-      {isAnnouncementOpen && <Announcement setIsAnnouncementOpen={setIsAnnouncementOpen} />}
       <Header ref={hamburgerRef} toggleSidebar={toggleSidebar} />
       <Root>
-        {!hideLeftSidebar && <SideBar ref={sideBarRef} isOpen={isSideBarOpen} />}
+        {!hideLeftSidebar && (
+          <SideBar ref={sideBarRef} isOpen={isSideBarOpen} />
+        )}
         <StyledContainer
           bgColor="body"
           marginTop={marginTop}
@@ -62,8 +66,8 @@ const Layout: FC<LayoutProps> = ({ children, hideLeftSidebar, hideRightSidebar, 
 Layout.defaultProps = {
   hideLeftSidebar: false,
   hideRightSidebar: false,
-  containerMaxWidth: 'sm',
-  marginTop: 'md',
+  containerMaxWidth: "sm",
+  marginTop: "md",
 };
 
 export default Layout;
