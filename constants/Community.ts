5f7f7996aacbc5e3b03f2faba791e333a529fe55
John <johnacreps@gmail.com>
2023-03-23 14:01:51 -0500

    Updated favicon

--- constants/Community.ts
+++ constants/Community.ts
@@ -1,10 +1,8 @@
 export const DefaultCommunity = {
   communityName: "Open-Radio.Net",
-  communityLogo:
-    "https://res.cloudinary.com/dkkf9iqnd/image/upload/v1632978887/community/c572c15e-72ab-4a72-b955-8be12931af06.png",
+  communityLogo: "/favicon.png",
   communityLogoPublicId: "",
-  communitySocialImage:
-    "https://res.cloudinary.com/dkkf9iqnd/image/upload/v1634034591/community/social.jpg",
+  communitySocialImage: "/favicon.png",
   primaryColor: "#0084FF",
   isEmailVerificationRequired: false,
   facebookLoginEnabled: false,
